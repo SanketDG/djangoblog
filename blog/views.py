2d9fb279e2a9e6aef9925b3a2bc2ff1bbfa7db04
SanketDG <sanketdasgupta@gmail.com>
2015-01-26 19:27:09 +0530

    sort posts by descending date

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__isnull=False).order_by('published_date')
+        published_date__isnull=False).order_by('published_date').reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
