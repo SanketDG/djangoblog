7c4322414762cb2286025a6f4c574e5cc5fd6d85
SanketDG <sanketdasgupta@gmail.com>
2015-01-26 12:23:04 +0530

    follow pep8

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,9 @@ from django import forms
 
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
 
-	class Meta:
-		model = Post;
-		fields = ('title','text','published_date')
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
