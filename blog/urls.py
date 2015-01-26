50bc66850c99e1f922517ade4950b259300fb8b5
SanketDG <sanketdasgupta@gmail.com>
2015-01-26 12:42:33 +0530

    add publish button

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
                        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
                        url(r'^post/(?P<pk>[0-9]+)/edit/$',
                            views.post_edit, name='post_edit'),
+                       url(r'^post/(?P<pk>[0-9]+)/publish/$',
+                           views.post_publish, name='post_publish'),
                        )
